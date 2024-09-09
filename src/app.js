a445a9a086d24496340081e5ba529eee8a958976
Hitesh Joshi <joshihitesh090@gmail.com>
2024-09-09 11:56:57 +0530

    added morgan for logging

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import userRouter from './controllers/users/userController.js';
 import { connectMongooseDatabase } from './database/index.js';
+import morgan from 'morgan';
 
 const app = express();
 app.use(express.json());
+app.use(morgan('combined'));
 
 app.use('/users', userRouter);
 
